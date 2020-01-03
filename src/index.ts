d5efcb737e76cd4afeb84e94a92ffe5850259bca
cesar18pena <cesar18pena@gmail.com>
2020-01-03 19:30:00 -0400

    Feat: adding typescript and express startup

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+const app: express.Application = express();
+const port: number = 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
