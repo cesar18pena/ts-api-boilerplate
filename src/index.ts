6e9728b3151d2088d9d25bef191d360f5d265259
Cesar Peña <cesar18pena@gmail.com>
2020-01-03 20:37:50 -0400

    Feat: added nodemon husky and lintstaged for improve development experience (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app: express.Application = express();
-const port = 3000;
+const port: number = parseInt(process.env.PORT as string, 10) || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
