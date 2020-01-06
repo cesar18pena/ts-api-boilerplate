cf03c288aea5311c1a6fbe89d9d961f911909e39
Cesar Peña <cesar18pena@gmail.com>
2020-01-06 18:15:24 -0400

    Feat: adding jest for testing (#5)

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
+    'plugin:jest/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
---
+++ tests/unit/example.spec.ts
@@ -0,0 +1,8 @@
+describe('Testing numbers', () => {
+  it('should return the correct sum of numbers', () => {
+    const x = 1;
+    const y = 2;
+
+    expect(x + y).toBe(3);
+  });
+});
