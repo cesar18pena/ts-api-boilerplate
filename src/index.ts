c9755f067e8c576ae829e342432757da28760013
Cesar Pena <cesar18pena@gmail.com>
2020-01-07 10:49:19 -0400

    Feat: adding winston and morgan for better logging system

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express, {Request, Response} from 'express';
+import morgan from 'morgan';
+import {logger, morganOption} from './config/logger';
 
 const app: express.Application = express();
+
 const port: number = parseInt(process.env.PORT as string, 10) || 3000;
 
-app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
+app.use(morgan('combined', morganOption));
+app.get('/', (req: Request, res: Response) => res.json({name: 'Cesar Peña'}));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => logger.info(`Example app listening on port ${port}!`));
